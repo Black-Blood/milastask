bb81dedaa2ba1f06992cb24d4d0dd9376b276095
Savrak Bohdan <73792196+Black-Blood@users.noreply.github.com>
2021-06-22 21:26:07 +0300

    adding new section and changing menu script

--- js/script.js
+++ js/script.js
@@ -1,20 +1,35 @@
-let pageHeaderNavLinks = document.querySelectorAll("[data-section-id]")
-let activeLink
-document.addEventListener("scroll", () => {
-  pageHeaderNavLinks.forEach(link => {
-    let section = document.getElementById(link.getAttribute("data-section-id"))
+/*
+*
+* Start menu script
+*
+*/
+let sections = document.querySelectorAll("section");
+let sectionLinksMenu = document.querySelector(".page-header-nav")
+let sectionsPosition = {}
 
-    const sectionTop = section.offsetTop
-    const sectionHeight = section.clientHeight
-
-    if (link.classList.contains("active")) {
-      activeLink = link
-    }
-
-    if (pageYOffset >= (sectionTop - sectionHeight / 2) && !link.classList.contains("active")) {
-      activeLink.classList.remove("active")
-      activeLink = link
-      activeLink.classList.add("active")
-    }
-  })
+sections.forEach(section => {
+  sectionsPosition[section.offsetTop - 75 - window.screen.availHeight / 2] = section.id
 })
+
+document.addEventListener("scroll", () => {
+  let scrollPosition = document.documentElement.scrollTop || document.body.scrollTop
+
+  let activeSectionID
+  for (position in sectionsPosition) {
+    if (Number(position) <= scrollPosition) {
+      activeSectionID = sectionsPosition[position];
+    }
+  }
+
+  let activeLink = sectionLinksMenu.querySelector(`a[href*=${activeSectionID}]`)
+
+  if (!activeLink.classList.contains("active")) {
+    sectionLinksMenu.querySelector("a.active").classList.remove("active")
+    activeLink.classList.add("active")
+  }
+})
+/*
+*
+* End menu script
+*
+*/
